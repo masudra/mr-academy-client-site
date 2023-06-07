32fb421cfbf34df8433c8a434353304b32eeb80c
masudra <masudrana6278312@gmail.com>
2023-06-07 23:57:07 +0600

    Register form creat

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -4,6 +4,8 @@ import {
 import Main from "../LayOut/Main";
 import Home from "../Pages/Home/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Regster from "../Pages/RegsterAndLogin/Regster/Regster";
+import Login from "../Pages/RegsterAndLogin/Login/Login";
 
    export const router = createBrowserRouter([
     {
@@ -14,6 +16,15 @@ import ErrorPage from "../Pages/ErrorPage/ErrorPage";
         {
             path: '/',
             element: <Home></Home>,
+        },
+        {
+          path:'/login',
+          element: <Login></Login>,
+
+        },
+        {
+          path: '/regster',
+          element: <Regster></Regster>,
         }
       ]
     },
