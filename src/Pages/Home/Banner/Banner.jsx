c0eedf10e49884a707f0e6582901f8abdf8ae70c
masudra <masudrana6278312@gmail.com>
2023-06-14 02:24:43 +0600

    banner change

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,80 +1,81 @@
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import logo1 from '../../../../public/baseball baner.jpg'
-import logo2 from '../../../../public/bsket ball banner.jpg'
+import logo2 from '../../../../public/sports-–-tennis.jpg'
 import logo3 from '../../../../public/ice hokey baner.jpg'
-import logo4 from '../../../../public/snooker banner.jpg'
-import logo5 from '../../../../public/sports-–-tennis.jpg'
-import logo6 from '../../../../public/golf baner.jpg'
+// import logo4 from '../../../../public/snooker banner.jpg'
+import logo5 from '../../../../public/golf baner.jpg'
 
 const Banner = () => {
-    
+
 
     return (
-        <Carousel>
-        <div className="h-[600px]">
-            <img  src={logo1} />
-             <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'></h5>
-                            <p>Playing game fresh your mind</p>
-                            
+          
+
+
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={logo1} className="w-full" />
+                <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
+                                <h5 className='text-5xl font-bold'>Baseball </h5>
+                                <p>
+A game is an activity or sport usually involving skill, knowledge, or chance, in which you follow fixed rules and try to win against an opponent or to solve a puzzle. ... the wonderful game of Baseball . </p>
+
+                            </div>
                         </div>
-                    </div>
-        </div>
-        <div  className="h-[600px]">
-            <img src={logo2} />
-            <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'>English </h5>
-                            <p>This course is designed to develop students proficiency in speaking, reading, writing, and understanding English. </p>
-                            
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={logo2} className="w-full" />
+                <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
+                                <h5 className='text-5xl font-bold'>Tennis </h5>
+                                <p>
+A game is an activity or sport usually involving skill, knowledge, or chance, in which you follow fixed rules and try to win against an opponent or to solve a puzzle. ... the wonderful game of Tennis . </p>
+
+                            </div>
                         </div>
-                    </div>
-        </div>
-        <div  className="h-[600px]">
-            <img src={logo3} />
-            <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'>Spanish</h5>
-                            <p>This course is designed to develop students proficiency in speaking, reading, writing, and understanding Spanish. </p>
-                            
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={logo3} className="w-full" />
+                <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
+                                <h5 className='text-5xl font-bold'>Ice hockey </h5>
+                                <p>
+A game is an activity or sport usually involving skill, knowledge, or chance, in which you follow fixed rules and try to win against an opponent or to solve a puzzle. ... the wonderful game of Ice hockey . </p>
+
+                            </div>
                         </div>
-                    </div>
-        </div>
-        <div  className="h-[600px]">
-            <img src={logo4} />
-            <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'>Japan</h5>
-                            <p>This course is designed to develop students proficiency in speaking, reading, writing, and understanding Japan. </p>
-                            
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={logo5}className="w-full" />
+                <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
+                                <h5 className='text-5xl font-bold'>Golf </h5>
+                                <p>
+A game is an activity or sport usually involving skill, knowledge, or chance, in which you follow fixed rules and try to win against an opponent or to solve a puzzle. ... the wonderful game of Golf. </p>
+
+                            </div>
                         </div>
-                    </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+           
         </div>
-        <div  className="h-[600px]">
-            <img src={logo5} />
-            <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'>Korea</h5>
-                            <p>This course is designed to develop students proficiency in speaking, reading, writing, and understanding korea. </p>
-                            
-                        </div>
-                    </div>
-        </div>
-        <div  className="h-[600px]">
-            <img src={logo6} />
-            <div className="absolute  flex items-center h-full left-0 right-0 top-4 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='text-white space-y-7 ml-4 pl-12 w-1/2'>
-                            <h5 className='text-5xl font-bold'>Hindi </h5>
-                            <p>This course is designed to develop students proficiency in speaking, reading, writing, and understanding Hindi. </p>
-                            
-                        </div>
-                    </div>
-        </div>
-       
-    </Carousel>
     );
 };
 
--- src/Pages/Hook/useStudentSelected.jsx
+++ src/Pages/Hook/useStudentSelected.jsx
@@ -6,21 +6,11 @@ const useStudentSelected = email => {
  
     const {user,loading}=useAuth
     const [axiosSecure] = useAxiosSecure();
-    // const token =localStorage.getItem('access-token')
 
     const { refetch, data: studentselect=[] } = useQuery({
         queryKey: ['studentselect',user?.email],
         enabled:!!user?.email && !!localStorage.getItem('access-token'),
 
-        // queryFn: async()=>{
-        //   const res = await fetch(`http://localhost:5000/studentselect?email=${user?.email}`,{
-        //     headers:{
-              
-        //       authorization:`berarer${token}`
-        //     }
-        //   })
-        //   return res.json()
-        // }
         queryFn: async()=>{
           const res = await axiosSecure(`/studentselect?email=${user?.email}`)
           return res.data
