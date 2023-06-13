9fee18ecbdcee67e2ce74f5bb487dd2764485b6d
masudra <masudrana6278312@gmail.com>
2023-06-13 12:25:40 +0600

    create useAxiosSecure

--- src/Pages/Hook/useStudentSelected.jsx
+++ src/Pages/Hook/useStudentSelected.jsx
@@ -1,16 +1,27 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContex } from '../Provider/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
 
 const useStudentSelected = email => {
  
-    const {user}=useContext(AuthContex)
+    const {user}=useAuth()
+    const [axiosSecure] = useAxiosSecure()
+    // const token =localStorage.getItem('access-token')
 
     const { refetch, data: studentselect=[] } = useQuery({
         queryKey: ['studentselect',user?.email],
+        // queryFn: async()=>{
+        //   const res = await fetch(`http://localhost:5000/studentSelect?email=${user?.email}`,{
+        //     headers:{
+              
+        //       authorization:`berarer${token}`
+        //     }
+        //   })
+        //   return res.json()
+        // }
         queryFn: async()=>{
-          const res = await fetch(`http://localhost:5000/studentSelect?email=${user?.email}`)
-          return res.json()
+          const res = await axiosSecure(`/studentSelect?email=${user?.email}`)
+          return res.data
         }
       })
    return [studentselect,refetch]
