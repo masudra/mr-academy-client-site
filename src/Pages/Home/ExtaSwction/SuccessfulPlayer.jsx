1dfe0bbea7a4a6c6b5dde743f29ce6586ba419cc
masudra <masudrana6278312@gmail.com>
2023-06-14 02:50:54 +0600

    exta esction add

---
+++ src/Pages/Home/ExtaSwction/SuccessfulPlayer.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import player1 from '../../../../public/pl-golf.jpg'
+
+const SuccessfulPlayer = () => {
+    return (
+        <div>
+            <div className='text-center text-4xl mt-16 mb-10 text-sky-600 font-bold'>
+                <h1>Our Top Successful Players</h1>
+                <p className='text-xl mt-2'>They coursed us from here</p>
+            </div>
+           <div className='grid md:grid-cols-3 gap-5'>
+           <div >
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img className='h-96 w-96 p-4' src={player1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> <span className='font-bold'>Name:</span>Rory McIlroy</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Game:</span>Golf</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Medali:</span>25</h2>
+                        <h2 className="card-title"> <span className='font-bold'>Total Years:</span>30</h2>
+                        <h2 className="card-title"> <span className='font-bold'>From:</span>Bangladesh</h2>
+                        <p>Golf is the best game populer game in the world</p>
+
+                    </div>
+                </div>
+            </div>
+
+           </div>
+
+
+
+        </div>
+    );
+};
+
+export default SuccessfulPlayer;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
+import SuccessfulPlayer from "../ExtaSwction/SuccessfulPlayer";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
                 <title>Mr Academy || Home </title>
             </Helmet>
             <Banner></Banner>
+            <SuccessfulPlayer></SuccessfulPlayer>
         </div>
     );
 };
